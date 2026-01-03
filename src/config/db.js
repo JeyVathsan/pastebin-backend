a08d8f06dc016ef1871cb3f22699db7352b29737
Jey Vathsan <ks.jeyvathsan@gmail.com>
2026-01-04 02:01:28 +0530

    Initial Commit

--- src/config/db.js
+++ src/config/db.js
@@ -1,13 +1,12 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: Number(process.env.DB_PORT) // 👈 important
+  host: process.env.MYSQLHOST || process.env.DB_HOST,
+  user: process.env.MYSQLUSER || process.env.DB_USER,
+  password: process.env.MYSQLPASSWORD || process.env.DB_PASSWORD,
+  database: process.env.MYSQLDATABASE || process.env.DB_NAME,
+  port: Number(process.env.MYSQLPORT || process.env.DB_PORT)
 });
-
 db.connect((err) => {
   if (err) {
     console.error("❌ MySQL connection failed:", err);
